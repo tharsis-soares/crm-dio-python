e620b6a4a5ff6da42e30df8578f052d5de039ebc
tharsis-soares <tharsissoares@hotmail.com>
2022-01-08 13:33:11 -0300

    teste heroku

--- crm/settings.py
+++ crm/settings.py
@@ -136,3 +136,6 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
